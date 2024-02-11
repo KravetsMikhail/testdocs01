6541ca092603bcbbfecb34f2f6766f3dc542e9c3
Kravets Mikhail <kravetsmihail@gmail.com>
2024-02-11 21:59:21 +0300

    Обновление

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Тест документации'
+project = 'Документация'
 copyright = '2024, kr'
 author = 'kr'
 
