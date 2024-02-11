e7a32f26932a00f9bf842599fa7ea29a8874cb18
Kravets Mikhail <kravetsmihail@gmail.com>
2024-02-11 21:33:43 +0300

    Второй релиз

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # testdocs01
 Пример документации на Sphinx
+
+Сборка проекта
+sphinx-autobuild D:\PROJECTS\rasu\docs\testdocs01\source D:\PROJECTS\rasu\docs\testdocs01\build\html
+
--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'testdocs01'
+project = 'Тест документации'
 copyright = '2024, kr'
 author = 'kr'
 
