56b0e9fd414451482f934b8a9350381647ca4ed0
KravetsMikhail <145544017+KravetsMikhail@users.noreply.github.com>
2024-02-22 15:37:40 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -17,10 +17,7 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-  'sphinx.ext.graphviz',
-  'sphinxcontrib.plantuml'
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
